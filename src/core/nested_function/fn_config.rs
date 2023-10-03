d639c8ff9f6a188e8e9500c504ac11e2f3129ef7
a-givertzman <a.givertzman@icloud.com>
2023-10-03 20:57:53 +0300

    FnConf tested valid cofigurations

--- src/core/nested_function/fn_config.rs
+++ src/core/nested_function/fn_config.rs
@@ -1,4 +1,4 @@
-use log::trace;
+use log::{trace, debug};
 use std::{fs, collections::HashMap, str::FromStr};
 
 use crate::core::nested_function::fn_conf_keywd::FnConfKeywd;
@@ -25,14 +25,30 @@ impl FnConfig {
     ///             input: 
     ///                 fn functionName:
     ///                 input: point '/path/Point.Name/'```
-    pub fn new(conf: &serde_yaml::Value) -> FnConfig {
+    pub fn new(conf: &serde_yaml::Value, vars: &mut Vec<String>) -> FnConfig {
         trace!("FnConfig.new | conf: {:?}", conf);
         if conf.is_string() {
             trace!("FnConfig.new | IS STRING");
-            let fnConfStr: String = serde_yaml::from_value(conf.clone()).unwrap();
-            let fnKeyword = FnConfKeywd::from_str(fnConfStr.as_str()).unwrap();
-            trace!("FnConfig.new | key: '{}'   |   fnKeyword: {:?}   |   fnConfStr: {:?}", "---", fnKeyword, fnConfStr);
-            FnConfig { fnType: fnKeyword.type_(), name: fnKeyword.name(), inputs: HashMap::new() }
+            match serde_yaml::from_value::<String>(conf.clone()) {
+                Ok(fnConfStr) => {
+                    match FnConfKeywd::from_str(fnConfStr.as_str()) {
+                        Ok(fnKeyword) => {                            
+                            trace!("FnConfig.new | key: '{}'   |   fnKeyword: {:?}   |   fnConfStr: {:?}", "---", fnKeyword, fnConfStr);
+                            FnConfig { fnType: fnKeyword.type_(), name: fnKeyword.name(), inputs: HashMap::new() }        
+                        },
+                        Err(err) => {
+                            let confStr = conf.as_str().unwrap().to_string();
+                            if vars.contains(&confStr) {
+                                debug!("FnConfig.new | kewword '{:?}' - found in variables", &conf);
+                            }        
+                            // panic!("Error parsing config: {:?}\n\terror: {:?}", conf, err)
+                        },
+                    }
+                },
+                Err(err) => {
+                    panic!("Error parsing config: {:?}\n\terror: {:?}", conf, err)
+                },
+            }
         } else {
             trace!("FnConfig.new | IS MAP");
             let mut fnType: FnConfigType = FnConfigType::Unknown;
@@ -47,7 +63,7 @@ impl FnConfig {
                         trace!("FnConfig.new | key: '{}'   |   fnKeyword: {:?}   |   inputfnConfMap: {:?}", key, fnKeyword, &inputfnConfMap);
                         fnType = fnKeyword.type_();
                         fnName = fnKeyword.name();
-                        for (inputName, inputConf) in Self::parseInputs(&inputfnConfMap) {
+                        for (inputName, inputConf) in Self::parseInputs(&inputfnConfMap, vars) {
                             inputs.insert(inputName, inputConf);
                         }
                     },
@@ -55,6 +71,10 @@ impl FnConfig {
                         panic!("FnConfig.new | NO KEYWD\n\tkey: {}  conf: {:?}\n\t error: {}", key, inputfnConfMap, err);
                     },
                 };
+            }
+            if fnType == FnConfigType::Var {
+                debug!("FnConfig.new | VAR created: {:?}", fnName);
+                vars.push(fnName.clone())
             }
             trace!("FnConfig.new | fnType: {:?} \t|\t fnName: {:?} \t|\t inputs: {:?}", &fnType, fnName, inputs);
             FnConfig {fnType, name: fnName, inputs}
@@ -92,12 +112,12 @@ impl FnConfig {
     ///         fn fnName...
     /// - input1: const 'val...'
     /// - input1: point '/path...'
-    fn parseInputs(conf: &serde_yaml::Value) -> Vec<(String, FnConfig)> {
+    fn parseInputs(conf: &serde_yaml::Value, vars: &mut Vec<String>) -> Vec<(String, FnConfig)> {
         trace!("FnConfig.parseInput | ENTER");
         let mut inputs = vec![];
         let inputsConfMap: HashMap<String, serde_yaml::Value> = serde_yaml::from_value(conf.clone()).unwrap();
         for (inputName, inputConfMap) in inputsConfMap {
-            let inputConf = FnConfig::new(&inputConfMap);
+            let inputConf = FnConfig::new(&inputConfMap, vars);
             inputs.push((inputName, inputConf));
         };
         inputs
@@ -106,11 +126,12 @@ impl FnConfig {
     /// reads config from path
     #[allow(dead_code)]
     pub fn read(path: &str) -> FnConfig {
+        let mut vars = vec![];
         match fs::read_to_string(&path) {
             Ok(yamlString) => {
                 match serde_yaml::from_str(&yamlString) {
                     Ok(config) => {
-                        FnConfig::new(&config)
+                        FnConfig::new(&config, &mut vars)
                     },
                     Err(err) => {
                         panic!("Error in config: {:?}\n\terror: {:?}", yamlString, err)
--- src/main.rs
+++ src/main.rs
@@ -23,12 +23,14 @@ fn main() {
     info!("test_create_valid_fn");
     // let (initial, switches) = initEach();
     let testData = [
-        (serde_yaml::from_str(r#"let newVar:
-            input1:
-                fn count:
-                    inputConst1: const '13.5'
-                    inputConst2: const '13.5'
-                    "#), FnConfig{ fnType: FnConfigType::Const, name: "".to_string(), inputs: HashMap::new() }),
+        (serde_yaml::from_str(r#"
+            let newVar1:
+                input1:
+                    fn count:
+                        inputConst1: const '13.5'
+                        inputConst2: newVar1
+        "#), 
+        FnConfig{ fnType: FnConfigType::Const, name: "".to_string(), inputs: HashMap::new() }),
                     // input2:
                     //     fn count:
                     //         inputConst1: const '13.5'
@@ -42,8 +44,10 @@ fn main() {
         debug!("value: {:?}   |   conf: {:?}   |   target: {:?}", "_", conf, target);
         // let fnKeyword = FnConfigKeyword::from_str(conf.as_str().unwrap()).unwrap();
         // debug!("\tfnKeyword: {:?}", fnKeyword);
-        let fnConfig = FnConfig::new(&conf);
+        let mut vars = vec![];
+        let fnConfig = FnConfig::new(&conf, &mut vars);
         debug!("\tfnConfig: {:?}", fnConfig);
+        debug!("\tvars: {:?}", vars);
         // assert_eq!(fnConfigType, target);
     }
 
