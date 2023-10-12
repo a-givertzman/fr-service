02a4f52429182d1e59a7993546014c3e073ccee9
a-givertzman <a.givertzman@icloud.com>
2023-10-12 14:56:36 +0300

    TaskCycle test

--- src/core_/aprox_eq/aprox_eq.rs
+++ src/core_/aprox_eq/aprox_eq.rs
@@ -1,8 +1,10 @@
+///
+/// provide approximately equality for f36, f64
 pub trait AproxEq<T> {
     fn aproxEq(&self, other: T, decimals: usize) -> bool;
 }
-
-
+///
+/// 
 impl AproxEq<f32> for f32 {
     fn aproxEq(&self, other: f32, decimals: usize) -> bool {
         let factor = 10.0f64.powi(decimals as i32) as f32;
@@ -11,7 +13,8 @@ impl AproxEq<f32> for f32 {
         a == b
     }
 }
-
+///
+/// 
 impl AproxEq<f64> for f64 {
     fn aproxEq(&self, other: f64, decimals: usize) -> bool {
         let factor = 10.0f64.powi(decimals as i32);
