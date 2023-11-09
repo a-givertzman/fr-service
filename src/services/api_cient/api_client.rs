63d54a8c6d0cc48a3091c39bd264e7e2a816c32e
a-givertzman <a.givertzman@icloud.com>
2023-11-09 14:56:36 +0300

    ApiClient run

--- src/services/api_cient/api_client.rs
+++ src/services/api_cient/api_client.rs
@@ -1,6 +1,6 @@
 #![allow(non_snake_case)]
 
-use std::{sync::{mpsc::{Receiver, Sender, self}, Arc, atomic::{AtomicBool, Ordering}}, time::Duration, thread};
+use std::{sync::{mpsc::{Receiver, Sender, self}, Arc, atomic::{AtomicBool, Ordering}}, time::Duration, thread, collections::VecDeque};
 
 use log::{info, debug, trace, warn};
 
@@ -42,6 +42,22 @@ impl ApiClient {
     }
     ///
     /// 
+    fn readQueue(selfName: &str, recv: &Receiver<PointType>, buffer: &mut VecDeque<PointType>) {
+        for _ in 0..1000 {                    
+            match recv.recv() {
+                Ok(point) => {
+                    debug!("ApiClient({}).run | point: {:?}", selfName, &point);
+                    buffer.push_front(point);
+                },
+                Err(_err) => {
+                    break;
+                    // warn!("ApiClient({}).run | Error receiving from queue: {:?}", selfName, err);
+                },
+            };
+        }
+    }
+    ///
+    /// 
     pub fn run(&mut self) {
         info!("ApiClient({}).run | starting...", self.id);
         let selfName = self.id.clone();
@@ -52,24 +68,18 @@ impl ApiClient {
             None => (false, Duration::ZERO),
         };
         let conf = self.conf.clone();
-        let mut recv = self.recv.pop().unwrap();
+        let recv = self.recv.pop().unwrap();
         let _h = thread::Builder::new().name("name".to_owned()).spawn(move || {
+            let mut buffer = VecDeque::new();
             let mut cycle = ServiceCycle::new(cycleInterval);
             'main: loop {
                 cycle.start();
-                trace!("ApiClient({}).run | calculation step...", selfName);
-                match recv.recv() {
-                    Ok(point) => {
-                        debug!("ApiClient({}).run | point: {:?}", selfName, &point);
-                    },
-                    Err(err) => {
-                        warn!("ApiClient({}).run | Error receiving from queue: {:?}", selfName, err);
-                    },
-                };
+                trace!("ApiClient({}).run | step...", selfName);
+                Self::readQueue(&selfName, &recv, &mut buffer);
                 if exit.load(Ordering::SeqCst) {
                     break 'main;
                 }
-                trace!("ApiClient({}).run | calculation step - done ({:?})", selfName, cycle.elapsed());
+                trace!("ApiClient({}).run | step - done ({:?})", selfName, cycle.elapsed());
                 if cyclic {
                     cycle.wait();
                 }
