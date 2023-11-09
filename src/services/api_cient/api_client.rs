444afb1d6ef2ad1b86ab1a175f0827018457606e
a-givertzman <a.givertzman@icloud.com>
2023-11-09 15:17:27 +0300

    ApiClient send

--- src/services/api_cient/api_client.rs
+++ src/services/api_cient/api_client.rs
@@ -1,6 +1,6 @@
 #![allow(non_snake_case)]
 
-use std::{sync::{mpsc::{Receiver, Sender, self}, Arc, atomic::{AtomicBool, Ordering}}, time::Duration, thread, collections::VecDeque};
+use std::{sync::{mpsc::{Receiver, Sender, self}, Arc, atomic::{AtomicBool, Ordering}}, time::Duration, thread, collections::VecDeque, net::TcpStream};
 
 use log::{info, debug, trace, warn};
 
@@ -42,18 +42,29 @@ impl ApiClient {
     }
     ///
     /// 
-    fn readQueue(selfName: &str, recv: &Receiver<PointType>, buffer: &mut VecDeque<PointType>) {
+    fn readQueue(selfName: &str, recv: &Receiver<PointType>, buffer: &mut Vec<PointType>) {
         for _ in 0..1000 {                    
             match recv.recv() {
                 Ok(point) => {
                     debug!("ApiClient({}).run | point: {:?}", selfName, &point);
-                    buffer.push_front(point);
+                    buffer.push(point);
                 },
                 Err(_err) => {
                     break;
                     // warn!("ApiClient({}).run | Error receiving from queue: {:?}", selfName, err);
                 },
             };
+        }
+    }
+    ///
+    /// Writing sql string to the TcpStream
+    fn send(selfName: &str, sql: String, stream: &mut TcpStream) -> Result<(), Box<dyn std::error::Error>>{
+        match stream.write(bytes) {
+            Ok(_) => Ok(()),
+            Err(err) => {
+                warn!("ApiClient({}).run | write to tcp stream error: {:?}", selfName, err);
+                Err(Box::new(err))
+            },
         }
     }
     ///
@@ -70,12 +81,28 @@ impl ApiClient {
         let conf = self.conf.clone();
         let recv = self.recv.pop().unwrap();
         let _h = thread::Builder::new().name("name".to_owned()).spawn(move || {
-            let mut buffer = VecDeque::new();
+            let mut buffer = Vec::new();
             let mut cycle = ServiceCycle::new(cycleInterval);
             'main: loop {
                 cycle.start();
                 trace!("ApiClient({}).run | step...", selfName);
                 Self::readQueue(&selfName, &recv, &mut buffer);
+                let mut count = buffer.len();
+                while count > 0 {
+                    match buffer.pop() {
+                        Some(point) => {
+                            let sql = point.asString();
+                            match Self::send(sql) {
+                                Ok(_) => {},
+                                Err(err) => {
+                                    warn!("ApiClient({}).run | error sending API: {:?}", selfName, err);
+                                },
+                            }
+                        },
+                        None => {},
+                    };
+                    count -=1;
+                }
                 if exit.load(Ordering::SeqCst) {
                     break 'main;
                 }
@@ -94,5 +121,4 @@ impl ApiClient {
     pub fn exit(&self) {
         self.exit.store(true, Ordering::SeqCst);
     }
-
 }
