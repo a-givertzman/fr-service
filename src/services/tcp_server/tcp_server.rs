e423cf16e2d870553ecec433c7f4b51460523d8e
a-givertzman <a.givertzman@icloud.com>
2023-12-15 12:13:14 +0300

    TcpServer setup incoming connection

--- src/services/tcp_server/tcp_server.rs
+++ src/services/tcp_server/tcp_server.rs
@@ -1,8 +1,8 @@
 #![allow(non_snake_case)]
 
-use std::{sync::{Arc, Mutex, atomic::{AtomicBool, Ordering}, mpsc::Sender}, thread::{self, JoinHandle}, net::TcpListener};
+use std::{sync::{Arc, Mutex, atomic::{AtomicBool, Ordering}, mpsc::Sender}, thread::{self, JoinHandle}, net::{TcpListener, TcpStream}, io::Read};
 
-use log::{info, warn};
+use log::{info, warn, debug};
 
 use crate::{
     services::{services::Services, service::Service}, 
@@ -33,6 +33,31 @@ impl TcpServer {
             exit: Arc::new(AtomicBool::new(false)),
         }
     }
+    ///
+    /// Setup thread for incomming connection
+    fn setupConnection(selfId: String, mut tcpStream: TcpStream, exit: Arc<AtomicBool>) -> Result<JoinHandle<()>, std::io::Error> {
+        info!("{}.setupConnection | starting...", selfId);
+        let selfIdClone = selfId.clone();
+        info!("{}.setupConnection | Preparing thread...", selfId);
+        let handle = thread::Builder::new().name(format!("{}.setupConnection", selfId.clone())).spawn(move || {
+            loop {
+                let mut bytes = vec![];
+                match tcpStream.read(&mut bytes) {
+                    Ok(len) => {
+                        debug!("{}.setupConnection | received {} bytes", selfId, len);
+                    },
+                    Err(err) => {
+                        debug!("{}.setupConnection | error: {:?}", selfId, err);
+                    },
+                };
+                if exit.load(Ordering::SeqCst) {
+                    break;
+                }
+            }
+        });
+        info!("{}.setupConnection | started", selfIdClone);
+        handle
+    }    
 }
 ///
 /// 
@@ -56,16 +81,34 @@ impl Service for TcpServer {
     fn run(&mut self) -> Result<JoinHandle<()>, std::io::Error> {
         info!("{}.run | starting...", self.id);
         let selfId = self.id.clone();
+        let conf = self.conf.clone();
         let exit = self.exit.clone();
         info!("{}.run | Preparing thread...", selfId);
         let handle = thread::Builder::new().name(format!("{}.run", selfId.clone())).spawn(move || {
+            let mut handles = vec![];
             loop {
-                match TcpListener::bind("127.0.0.1:80") {
+                match TcpListener::bind(conf.address) {
                     Ok(listener) => {
-                        
+                        for stream in listener.incoming() {
+                            match stream {
+                                Ok(stream) => {
+                                    match Self::setupConnection(selfId.clone(), stream, exit.clone()) {
+                                        Ok(handle) => {
+                                            handles.push(handle);
+                                        },
+                                        Err(err) => {
+                                            warn!("{}.run | error: {:?}", selfId, err);
+                                        },
+                                    };
+                                },
+                                Err(err) => {
+                                    warn!("{}.run | error: {:?}", selfId, err);
+                                },
+                            }
+                        }                        
                     },
                     Err(err) => {
-                        warn!("{}.run | Preparing thread...", selfId);
+                        warn!("{}.run | error: {:?}", selfId, err);
                     },
                 };
                 if exit.load(Ordering::SeqCst) {
