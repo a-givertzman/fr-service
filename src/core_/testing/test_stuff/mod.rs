8475ab894441555f39102c3996dd963908b1e245
a-givertzman <a.givertzman@icloud.com>
2023-12-18 19:22:38 +0300

    TcpServer test

--- src/core_/testing/test_stuff/mod.rs
+++ src/core_/testing/test_stuff/mod.rs
@@ -3,3 +3,5 @@ pub mod test_value;
 pub mod random_test_values;
 
 pub mod max_test_duration;
+
+pub mod inc_test_values;
--- src/tests/unit/services/tcp_server/tcp_server_test.rs
+++ src/tests/unit/services/tcp_server/tcp_server_test.rs
@@ -3,7 +3,7 @@
 mod tests {
     use log::{warn, info, debug};
     use std::{sync::{Once, Arc, Mutex}, time::{Duration, Instant}, thread};
-    use crate::{core_::{debug::debug_session::{DebugSession, LogLevel, Backtrace}, testing::test_stuff::max_test_duration::MaxTestDuration}, conf::tcp_server_config::TcpServerConfig, services::{tcp_server::tcp_server::TcpServer, services::Services, service::Service}, tests::unit::services::tcp_server::mock_tcp_client::MockTcpClient}; 
+    use crate::{core_::{debug::debug_session::{DebugSession, LogLevel, Backtrace}, testing::test_stuff::{max_test_duration::MaxTestDuration, inc_test_values::IncTestValues, test_value::Value}}, conf::tcp_server_config::TcpServerConfig, services::{tcp_server::tcp_server::TcpServer, services::Services, service::Service}, tests::unit::services::tcp_server::mock_tcp_client::MockTcpClient}; 
     
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     // use super::*;
@@ -28,7 +28,7 @@ mod tests {
     }
     
     #[test]
-    fn test_tcp_server() {
+    fn test_tcp_server_send() {
         DebugSession::init(LogLevel::Debug, Backtrace::Short);
         initOnce();
         initEach();
@@ -37,6 +37,17 @@ mod tests {
         let selfId = "test";
         let maxTestDuration = MaxTestDuration::new(selfId, Duration::from_secs(10));
         maxTestDuration.run().unwrap();
+
+        let iterations = 100;
+        let testData = IncTestValues::new(
+            selfId, 
+            0, 
+            iterations, 
+        );
+        let testData: Vec<Value> = testData.collect();
+        let totalCount = testData.len();
+
+        
         let conf = r#"
             service TcpServer:
                 cycle: 1 ms
@@ -60,11 +71,21 @@ mod tests {
         services.lock().unwrap().insert("TcpServer", tcpServer.clone());
         let handle = tcpServer.lock().unwrap().run().unwrap();
         thread::sleep(Duration::from_millis(1000));
+
+        let received = mockTcpClient.lock().unwrap().received();
+        let received = received.lock().unwrap();
+        let target = totalCount;
+        let result = received.len();
+        assert!(result == target, "\nresult: {:?}\ntarget: {:?}", result, target);
+        for (index, point) in received.iter().enumerate() {
+            let result = point.asInt().value;
+            let target = index as i64;
+            assert!(result == target, "\nresult: {:?}\ntarget: {:?}", result, target);
+        }
+
+
         tcpServer.lock().unwrap().exit();
         handle.join().unwrap();
-        let target = true;
-        let result = true;
-        assert!(result == target, "\nresult: {:?}\ntarget: {:?}", result, target);
         maxTestDuration.exit();
     }
 }
