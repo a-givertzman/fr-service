f58c87df82b34dca8e057cd88ccf1604bae1c701
a-givertzman <a.givertzman@icloud.com>
2024-01-12 19:21:00 +0300

    Config | Auth

--- src/conf/conf_tree.rs
+++ src/conf/conf_tree.rs
@@ -157,9 +157,13 @@ impl ConfTree {
     /// removes node by it's key if exists
     /// returns Result<&Self>
     pub fn remove(&mut self, key: &str) -> Result<serde_yaml::Value, String> {
-        match self.conf.as_mapping_mut().unwrap().remove(key) {
-            Some(value) => Ok(value),
-            None => Err(format!("Key '{:?}' not found in the node '{:?}'", &key, &self.conf)),
+        if self.conf.is_mapping() {
+            match self.conf.as_mapping_mut().unwrap().remove(key) {
+                Some(value) => Ok(value),
+                None => Err(format!("Key '{:?}' not found in the node '{:?}'", &key, &self.conf)),
+            }
+        } else {
+            Err(format!("Key '{:?}' not found in the node '{:?}'", &key, &self.conf))
         }
     }
 }
