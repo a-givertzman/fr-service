5e80c818ab658809bdffb3b9c30bd319f67b4cb4
a-givertzman <a.givertzman@icloud.com>
2024-02-01 18:30:51 +0300

    ProfinetClient | S7Client

--- src/services/profinet_client/mod.rs
+++ src/services/profinet_client/mod.rs
@@ -1,3 +1,5 @@
 pub mod profinet_client;
 
 pub mod profinet_db;
+
+pub mod s7;
--- src/services/profinet_client/profinet_db.rs
+++ src/services/profinet_client/profinet_db.rs
@@ -3,11 +3,14 @@
 use std::time::Duration;
 
 use indexmap::IndexMap;
+use log::{debug, warn};
 
-use crate::conf::{point_config::point_config::PointConfig, profinet_client_config::profinet_db_config::ProfinetDbConfig};
+use crate::{conf::{point_config::point_config::PointConfig, profinet_client_config::profinet_db_config::ProfinetDbConfig}, core_::point::point_type::PointType};
+
+use super::s7::s7_client::S7Client;
 
 ///
-/// 
+/// Represents PROFINET DB - a collection of the PROFINET addresses
 pub struct ProfinetDb {
     id: String,
     pub name: String,
@@ -36,5 +39,90 @@ impl ProfinetDb {
                 (pointConf.name.clone(), pointConf.clone())
             }).collect(),
         }
+    }
+    ///
+    /// 
+    pub fn read(&self, client: &S7Client) -> Result<Vec<PointType>, String> {
+        if client.isConnected {
+            debug!(
+                "{}.read | reading DB: {:?}, offset: {:?}, size: {:?}",
+                self.id, self.number, self.offset, self.size
+            );
+            match client.read(self.number, self.offset, self.size) {
+                Ok(bytes) => {
+                    // let bytes = client.read(899, 0, 34).unwrap();
+                    // print!("\x1B[2J\x1B[1;1H");
+                    // debug!("{:?}", bytes);
+                    for (_key, pointType) in self.points {
+                        // match pointType.clone() {
+                        //     ParsePointType::Bool(mut point) => {
+                        //         point.addRaw(&bytes);
+                        //         // debug!("{} parsed point Bool: {:?}", logPref, point);
+                        //         if point.isChanged() {
+                        //             let dsPoint = DsPoint::newBool(
+                        //                 point.name.as_str(),
+                        //                 false,
+                        //                 DsStatus::Ok,
+                        //                 point.timestamp,
+                        //                 point.h,
+                        //                 point.a,
+                        //             );
+                        //             // sender.push(value)
+                        //             debug!(
+                        //                 "{} point Bool: {:?}",
+                        //                 logPref, dsPoint.value
+                        //             );
+                        //         }
+                        //     }
+                        //     ParsePointType::Int(mut point) => {
+                        //         point.addRaw(&bytes);
+                        //         // debug!("{} parsed point Int: {:?}", logPref, point);
+                        //         if point.isChanged() {
+                        //             let dsPoint = DsPoint::newInt(
+                        //                 point.name.as_str(),
+                        //                 0,
+                        //                 DsStatus::Ok,
+                        //                 point.timestamp,
+                        //                 point.h,
+                        //                 point.a,
+                        //             );
+                        //             // sender.push(value)
+                        //             debug!(
+                        //                 "{} point Int: {:?}",
+                        //                 logPref, dsPoint.value
+                        //             );
+                        //         }
+                        //     }
+                        //     ParsePointType::Real(mut point) => {
+                        //         point.addRaw(&bytes);
+                        //         // debug!("{} parsed point Real: {:?}", logPref, point);
+                        //         if point.isChanged() {
+                        //             let dsPoint = DsPoint::newReal(
+                        //                 point.name.as_str(),
+                        //                 point.value,
+                        //                 DsStatus::Ok,
+                        //                 point.timestamp,
+                        //                 point.h,
+                        //                 point.a,
+                        //             );
+                        //             // debug!("{} point (Real): {:?} {:?}", logPref, dsPoint.name, dsPoint.value);
+                        //             sender.push(dsPoint).unwrap();
+                        //         }
+                        //     }
+                        // }
+                    }
+                    Ok(vec![])
+                }
+                Err(err) => {
+                    let message = format!("{}.read | read error: {}", self.id, err);
+                    warn!("{}", message);
+                    Err(message)
+                }
+            }
+        } else {
+            let message = format!("{}.read | read error: {} - is not connected", self.id, client.id);
+            warn!("{}", message);
+            Err(message)
+        }        
     }
 }
