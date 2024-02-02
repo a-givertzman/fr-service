90b00984084d06f6f826cf89dab4f565079806af
a-givertzman <a.givertzman@icloud.com>
2024-02-02 16:31:23 +0300

    ProfinetClient test

--- src/services/profinet_client/s7/s7_lib.rs
+++ src/services/profinet_client/s7/s7_lib.rs
@@ -1,17 +1,20 @@
+use std::env;
+
 use once_cell::sync::Lazy;
 use snap7_sys::LibSnap7;
 
 pub static S7LIB: Lazy<LibSnap7> = Lazy::new(|| {
     println!("initializing LibSnap7 lib...");
     let paths = [
-        "./libsnap7.so",
-        "/usr/lib/libsnap7.so",
+        format!("{}/libsnap7.so", env::current_dir().unwrap().display()),
+        format!("/usr/lib/libsnap7.so"),
     ];
     for path in paths {
         println!("check '{}'...", path);
-        match unsafe { LibSnap7::new(path) } {
+        match unsafe { LibSnap7::new(&path) } {
             Ok(lib) => {
-                println!("initializing LibSnap7 lib - ok ({})", path);
+                println!("check '{}' - ok", path);
+                println!("initializing LibSnap7 lib - ok");
                 return lib;
             },
             Err(_) => {
