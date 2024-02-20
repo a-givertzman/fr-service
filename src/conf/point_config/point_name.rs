865c5ecead5a5ce80bfb6af900fd31e178ff5d91
a-givertzman <a.givertzman@icloud.com>
2024-02-20 17:15:47 +0300

    ProfinetClient | write

--- src/conf/point_config/point_name.rs
+++ src/conf/point_config/point_name.rs
@@ -1,4 +1,11 @@
-struct PointName {
+///
+/// Creates Point name from parts, dividing parts with single "/" character
+/// - path1 + "" = /path1
+/// - path1 + path2 = /path1/path2
+/// - path1/ + path2 = /path1/path2
+/// - path1 + /path2 = /path1/path2
+/// - path1/ + /path2 = /path1/path2
+pub struct PointName {
     value: String
 }
 ///
@@ -6,21 +13,60 @@ struct PointName {
 impl PointName {
     ///
     /// 
-    pub fn new(parent: &str, name: &str) ->Self {
-        let value = match parent.chars().last() {
-            Some(last) => {
-                if last == '/' {
-                    format!("{}{}", parent, name)
+    pub fn new(left: &str, right: &str) ->Self {
+        let left = match left.chars().next() {
+            Some(left_first) => {
+                if left_first == '/' {
+                    format!("{}", left)
                 } else {
-                    format!("{}/{}", parent, name)
+                    format!("/{}", left)
                 }
             },
             None => {
-                format!("{}{}", parent, name)
+                format!("/")
+            },
+        };
+        let value = match left.chars().last() {
+            Some(left_last) => {
+                match right.chars().next() {
+                    Some(right_first) => {
+                        if left_last == '/' && right_first == '/' {
+                            format!("{}{}", left, &right[1..])
+                        } else if left_last == '/' && right_first != '/' {
+                            format!("{}{}", left, right)
+                        } else if left_last != '/' && right_first == '/' {
+                            format!("{}{}", left, right)
+                        } else {
+                            format!("{}/{}", left, right)
+                        }
+                    },
+                    None => {
+                        left
+                    },
+                }
+            },
+            None => {
+                match right.chars().next() {
+                    Some(name_first) => {
+                        if name_first == '/' {
+                            right.to_string()
+                        } else {
+                            format!("/{}", right)
+                        }
+                    },
+                    None => {
+                        panic!("PointName.new | Parent or name must not be empty")
+                    },
+                }
             },
         };
         Self {
             value,
         }
+    }
+    ///
+    /// 
+    pub fn full(&self) -> String {
+        self.value.clone()
     }
 }
