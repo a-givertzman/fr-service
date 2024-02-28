96b5143cfcb8a57096c6c3ab127f4690eb5ce597
a-givertzman <a.givertzman@icloud.com>
2024-02-28 16:33:06 +0300

    JdsService

---
+++ src/services/jds_service/mod.rs
@@ -0,0 +1,5 @@
+//!
+//! JdsService implements behavior on the JDS communication protocol for the following kinds of requests:
+//! - "Start" - after this request points transmission begins
+//! - "Points" - all points configurations requested
+//! - "Auth" request - authentication requested
--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,3 +1,14 @@
+//! ### Services implemented for the application
+//! **Service**:
+//! - executed in the separate thread, can be multi thread
+//! - basicaly must be defined in the main configuration file like:
+//! ```yaml
+//! service ServiceName Id:
+//!     in queue in-queue:
+//!         max-length: 10000
+//!     out queue: MultiQueue.in-queue
+//! ```
+///
 pub mod api_cient;
 
 pub mod tcp_client;
@@ -17,3 +28,5 @@ pub mod multi_queue;
 pub mod queue_name;
 
 pub mod tcp_server;
+
+pub mod jds_service;
