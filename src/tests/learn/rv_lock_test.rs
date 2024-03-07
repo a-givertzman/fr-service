2417fd86abbc6eff3489b422dd07bbc993bc0be5
a-givertzman <a.givertzman@icloud.com>
2024-03-07 21:07:05 +0300

    JdsService.subscribe

--- src/tests/learn/rv_lock_test.rs
+++ src/tests/learn/rv_lock_test.rs
@@ -261,7 +261,165 @@ mod tests {
         println!("Received events: {:?}", received.read().unwrap().len());
         
         test_duration.exit();
-    }    
+    }
+    ///
+    /// 
+    // #[ignore = "learn - all must be ignored"]
+    #[test]
+    fn matching() {
+        DebugSession::init(LogLevel::Debug, Backtrace::Short);
+        init_once();
+        println!("");
+        let self_id = "test access values using match";
+        println!("\n{}", self_id);
+        let test_duration = TestDuration::new(self_id, Duration::from_secs(10));
+        test_duration.run().unwrap();
+        let test_data = init_each(self_id);
+
+        let test_data_len = test_data.len();
+        let map = Arc::new(
+            Mutex::new(HashMap::with_hasher(BuildHasherDefault::<FxHasher>::default()))
+        );
+        let (send, recv) = mpsc::channel();
+        for key in KEYS {
+            map.lock().unwrap().insert(key, send.clone());
+        }
+        let received = Arc::new(RwLock::new(vec![]));
+        let received_clone = received.clone();
+        let receiver_handle = thread::Builder::new().name(format!("{} - Read", self_id)).spawn(move || {
+            let mut received_local = vec![];
+            let mut received_local_len = 0;
+            while received_local_len < test_data_len {
+                match recv.recv_timeout(Duration::from_secs(10)) {
+                    Ok(value) => {
+                        received_local.push(value);
+                        received_local_len += 1;
+                    },
+                    Err(err) => {
+                        error!("Error receiving value: {:?}", err);
+                    },
+                }
+            }
+            *received_clone.write().unwrap() = received_local;
+        }).unwrap();
+        let sent = Arc::new(RwLock::new(0));
+        let sent_clone = sent.clone();
+        let timer = Instant::now();
+        let sender_handle = thread::Builder::new().name(format!("{} - Read", self_id)).spawn(move || {
+            let mut sent_local = 0;
+            let mut key;
+            let mut key_iter = KEYS.iter().cycle();
+            for value in test_data {
+                key = key_iter.next().unwrap();
+                match *key {
+                    "stream_01" => {
+                        match send.send(value) {
+                            Ok(_) => {
+                                sent_local += 1;
+                            },
+                            Err(err) => error!("Error sending value to the sender '{:?}'", err),
+                        }
+                    },
+                    "stream_03" => {
+                        match send.send(value) {
+                            Ok(_) => {
+                                sent_local += 1;
+                            },
+                            Err(err) => error!("Error sending value to the sender '{:?}'", err),
+                        }
+                    },
+                    "stream_04" => {
+                        match send.send(value) {
+                            Ok(_) => {
+                                sent_local += 1;
+                            },
+                            Err(err) => error!("Error sending value to the sender '{:?}'", err),
+                        };
+                    },
+                    "stream_05" => {
+                        match send.send(value) {
+                            Ok(_) => sent_local += 1,
+                            Err(err) => error!("Error sending value to the sender '{:?}'", err),
+                        }
+                    },
+                    "stream_06" => {
+                        match send.send(value) {
+                            Ok(_) => sent_local += 1,
+                            Err(err) => error!("Error sending value to the sender '{:?}'", err),
+                        }
+                    },
+                    "stream_07" => {
+                        match send.send(value) {
+                            Ok(_) => sent_local += 1,
+                            Err(err) => error!("Error sending value to the sender '{:?}'", err),
+                        }
+                    },
+                    "stream_08" => {
+                        match send.send(value) {
+                            Ok(_) => sent_local += 1,
+                            Err(err) => error!("Error sending value to the sender '{:?}'", err),
+                        }
+                    },
+                    "stream_09" => {
+                        match send.send(value) {
+                            Ok(_) => sent_local += 1,
+                            Err(err) => error!("Error sending value to the sender '{:?}'", err),
+                        }
+                    },
+                    "stream_10" => {
+                        match send.send(value) {
+                            Ok(_) => sent_local += 1,
+                            Err(err) => error!("Error sending value to the sender '{:?}'", err),
+                        }
+                    },
+                    "stream_11" => {
+                        match send.send(value) {
+                            Ok(_) => sent_local += 1,
+                            Err(err) => error!("Error sending value to the sender '{:?}'", err),
+                        }
+                    },
+                    "stream_12" => {
+                        match send.send(value) {
+                            Ok(_) => sent_local += 1,
+                            Err(err) => error!("Error sending value to the sender '{:?}'", err),
+                        }
+                    },
+                    "stream_13" => {
+                        match send.send(value) {
+                            Ok(_) => sent_local += 1,
+                            Err(err) => error!("Error sending value to the sender '{:?}'", err),
+                        }
+                    },
+                    "stream_14" => {
+                        match send.send(value) {
+                            Ok(_) => sent_local += 1,
+                            Err(err) => error!("Error sending value to the sender '{:?}'", err),
+                        }
+                    },
+                    "stream_15" => {
+                        match send.send(value) {
+                            Ok(_) => sent_local += 1,
+                            Err(err) => error!("Error sending value to the sender '{:?}'", err),
+                        }
+                    },
+                    _ => panic!("Unknown key '{}'", key),
+                }
+            }
+            *sent_clone.write().unwrap() = sent_local;
+        }).unwrap();
+        
+        receiver_handle.wait().unwrap();
+        sender_handle.wait().unwrap();
+
+        println!("\n{}", self_id);
+        println!("Elapsed: {:?}", timer.elapsed());
+        println!("Elapsed per event: {:?}", timer.elapsed().div_f32(test_data_len as f32));
+        println!("Total test events: {:?}", test_data_len);
+        println!("Sent events: {:?}", sent.read().unwrap());
+        println!("Received events: {:?}", received.read().unwrap().len());
+        
+        test_duration.exit();
+    }
     const KEYS: [&str; 14] = [
         "stream_01",
         "stream_03",
