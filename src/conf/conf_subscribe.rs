5f0968b51770e1d7c0a96f31159ef569f409451b
a-givertzman <a.givertzman@icloud.com>
2024-03-20 20:00:46 +0300

    ConfSubscribe | test

--- src/conf/conf_subscribe.rs
+++ src/conf/conf_subscribe.rs
@@ -1,8 +1,7 @@
 use std::collections::HashMap;
+use log::trace;
 use crate::{core_::cot::cot::Cot, services::multi_queue::subscription_criteria::SubscriptionCriteria};
-
-use super::point_config::point_config::PointConfig;
-
+use super::point_config::{point_config::PointConfig, point_config_history::PointConfigHistory};
 ///
 /// Service Configuration, to be subscribed on some service / services, by number of criterias
 /// ------------------------------------------------------------------------------------------
@@ -45,68 +44,155 @@ impl ConfSubscribe {
         }
     }
     ///
-    /// 
+    /// Returns subscriptions based on the given points and number of configured criterias
     pub fn with(&self, points: &Vec<PointConfig>) -> HashMap<String, Option<Vec<SubscriptionCriteria>>> {
-        let point_configs = points;
         if self.conf.is_string() {
             let service = self.conf.as_str().unwrap().to_owned();
             HashMap::from([
                 (service, Some(vec![]))
             ])
         } else if self.conf.is_mapping() {
-            let mut subscriptions = HashMap::<String, Option<Vec<SubscriptionCriteria>>>::new();
-            for (service, criterias) in self.conf.as_mapping().unwrap() {
-                let service = service.as_str().unwrap();
-                let mut points = criterias.as_mapping().unwrap().into_iter().fold(vec![], |mut points, (options, names)| {
-                    let names = names.as_sequence().unwrap();
-                    let point_configs = if names.is_empty() {
-                        point_configs.clone()
-                    } else {
-                        names.iter().filter_map(|name| {
-                            let name: String = serde_yaml::from_value(name.clone()).unwrap();
-                            point_configs.iter().find(|&point_conf| point_conf.name == name)
-                        }).cloned().collect()
-                    };
-                    let creterias = point_configs.into_iter().filter_map(|point_conf| {
-                        if options.is_string() {
-                            let cot = serde_yaml::from_value(options.clone()).unwrap();
-                            Some(SubscriptionCriteria::new(point_conf.name, cot))
-                        } else if options.is_mapping() {
-                            let options = options.as_mapping().unwrap();
-                            let cot = options.get("cot").map(|v| serde_yaml::from_value(v.clone()).unwrap()).unwrap_or(Cot::All);
-                            let history = options.get("history").map(|v| serde_yaml::from_value(v.clone()).unwrap());
-                            let alarm = options.get("alarm").map(|v| v.as_u64().unwrap());
-                            let mut filter = false;
-                            if let Some(history) = history {
-                                filter |= point_conf.history != history
-                            }
-                            if let Some(alarm) = alarm {
-                                filter |= point_conf.alarm.map(|v| v != alarm as u8).unwrap_or(true);
-                            }
-                            if !filter {
-                                return Some(SubscriptionCriteria::new(point_conf.name, cot))
-                            }
-                            None
-                        } else {
-                            panic!("{}.new | Invalid subscribe options format: {:#?}", self.id, options);
-                        }
-                    });
-                    points.extend(creterias);
-                    points
-                });
-                // let points = if points.is_empty() {None} else {Some(points)};
-                subscriptions.entry(service.to_owned())
-                    .and_modify(|entry| {
-                        entry.as_mut().and_then(|v| {
-                            v.append(&mut points);
-                            Some(v)
-                        }).or(if points.is_empty() {None} else {Some(&mut points)});
+            match self.conf.as_mapping() {
+                Some(conf) => {
+                    conf.iter().fold(HashMap::new(), |mut subscriptions, (service, criterias_conf)| {
+                        let service = service.as_str().unwrap();
+                        let criterias = Criterias::new(&self.id, &criterias_conf, points).build();
+                        subscriptions
+                            .entry(service.to_owned())
+                            .and_modify(|entry| {
+                                if let Some(points) = &criterias {
+                                    match entry {
+                                        Some(entry) => entry.append(&mut points.clone()),
+                                        None => {
+                                            entry.replace(points.clone());
+                                        },
+                                    };
+                                }
+                            })
+                            .or_insert(criterias);
+                        subscriptions
                     })
-                    .or_insert(if points.is_empty() {None} else {Some(points)});
+                },
+                None => {
+                    panic!("{}.new | Yaml Mapping expected, but not found: {:#?}", self.id, self.conf);
+                },
             }
-            subscriptions
         } else {
-            panic!("{}.new | Invalid subscribe format: {:#?}", self.id, self.conf);
+            panic!("{}.new | Invalid Subscribe option format: {:#?}", self.id, self.conf);
         }
     }
+}
+
+///         Inf: []         #   - on all points having Cot::Inf
+///         {cot: Inf, history: r}: []  #   - on all points having Cot::Inf and history::read
+///         {cot: Inf, history: r}:         #   - on concrete points having Cot::Inf and history::read
+///             - /App/Service/Point.Name.1
+///             - /App/Service/Point.Name.2
+struct Criterias {
+    id: String,
+    conf: serde_yaml::Value,
+    points: Vec<PointConfig>
+}
+///
+/// 
+impl Criterias {
+    fn new(parent: &str, conf: &serde_yaml::Value, points: &Vec<PointConfig>) -> Self {
+        Self {
+            id: format!("{}/Criterias", parent),
+            conf: conf.clone(),
+            points: points.clone(),
+        }
+    }
+    ///
+    /// Returns SubscriptionCreteria's
+    fn build(&self) -> Option<Vec<SubscriptionCriteria>> {
+        match self.conf.as_mapping() {
+            Some(conf) => {
+                let mut points: Option<Vec<SubscriptionCriteria>> = None;
+                if conf.is_empty() {
+                    points = Some(vec![]);
+                } else {
+                    for (options, names) in conf {
+                        let criterias = Self::build_criterias(&self.id, options, names, &self.points);
+                        trace!("{}.new | criterias: {:?}", self.id, criterias);
+                        if let Some(mut criterias) = criterias {
+                            points = points
+                                .as_mut()
+                                .map_or(Some(criterias.clone()), |v| {
+                                    v.append(&mut criterias);
+                                    Some(v.to_vec())
+                                });
+                        }
+                        trace!("{}.new | points: {:?}", self.id, points);
+                    }
+                }
+                points
+            },
+            None => None,
+        }
+    }
+    ///
+    /// Creates list of Subscriptions based on the given point names, point configs, and filtering criterias
+    fn build_criterias(self_id: &str, options: &serde_yaml::Value, names: &serde_yaml::Value, point_configs: &Vec<PointConfig>) -> Option<Vec<SubscriptionCriteria>> {
+        trace!("{}.build_criterias | options: {:?}", self_id, options);
+        trace!("{}.build_criterias | names: {:?}", self_id, names);
+        let names = names.as_sequence().unwrap();
+        if options.is_string() {
+            let cot: Cot = serde_yaml::from_value(options.clone()).unwrap();
+            let point_configs = Self::build_point_configs(names, point_configs);
+            if point_configs.is_empty() {
+                Some(vec![])
+            } else {
+                Some(point_configs.iter().map(|conf| {
+                    SubscriptionCriteria::new(conf.name.clone(), cot)
+                }).collect())
+            }
+        } else if options.is_mapping() {
+            let options = options.as_mapping().unwrap();
+            let cot = options.get("cot").map(|v| serde_yaml::from_value(v.clone()).unwrap()).unwrap_or(Cot::All);
+            let alarm = options.get("alarm").map(|v| v.as_u64().unwrap());
+            let history = options.get("history").map(|v| serde_yaml::from_value(v.clone()).unwrap());
+            trace!("{}.build_criterias | names: {:?}", self_id, names);
+            let point_configs = Self::build_point_configs(names, point_configs);
+            let creterias = point_configs
+                .into_iter()
+                .filter_map(|point_conf| {
+                    Self::accept(&point_conf, &history, &alarm).then_some(SubscriptionCriteria::new(point_conf.name, cot))
+                });
+            if (&creterias).clone().peekable().peek().is_some() {
+                Some(creterias.collect())
+            } else {
+                None
+            }
+        } else {
+            panic!("{}.build_criterias | Invalid subscribe options format: {:#?}", self_id, options);
+        }
+    }
+    ///
+    /// Returns all configs if names is empty, 
+    /// otherwise returns configs for given names
+    fn build_point_configs(names: &Vec<serde_yaml::Value>, configs: &Vec<PointConfig>) -> Vec<PointConfig> {
+        if names.is_empty() {
+            configs.clone()
+        } else {
+            names.iter().filter_map(|name| {
+                let name: String = serde_yaml::from_value(name.clone()).unwrap();
+                configs.iter().find(|&point_conf| point_conf.name == name)
+            }).cloned().collect()
+        }
+    }    
+    ///
+    /// Returns true if point_config is accepted by the options:
+    ///     - alarm
+    ///     - history
+    fn accept(point_conf: &PointConfig, history: &Option<PointConfigHistory>, alarm: &Option<u64>) -> bool {
+        let mut accepted = true;
+        if let Some(history) = &history {
+            accepted &= point_conf.history == *history
+        }
+        if let Some(alarm) = alarm {
+            accepted &= point_conf.alarm.map(|v| v != (*alarm as u8)).unwrap_or(true);
+        }
+        accepted
+    }    
 }
