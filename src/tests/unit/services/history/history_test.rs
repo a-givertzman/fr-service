0977394f679414321a37f8782bc319fafdb396db
a-givertzman <a.givertzman@icloud.com>
2024-03-25 17:46:39 +0300

    History | config

--- src/tests/unit/services/history/history_test.rs
+++ src/tests/unit/services/history/history_test.rs
@@ -2,9 +2,11 @@
 
 mod history {
     use log::{warn, info, debug};
-    use std::{sync::Once, time::{Duration, Instant}};
+    use std::{sync::Once, env, time::{Duration, Instant}};
     use testing::stuff::max_test_duration::TestDuration;
     use debugging::session::debug_session::{DebugSession, LogLevel, Backtrace};
+
+    use crate::services::app::app::App;
     ///
     /// 
     static INIT: Once = Once::new();
@@ -32,6 +34,12 @@ mod history {
         println!("\n{}", self_id);
         let test_duration = TestDuration::new(self_id, Duration::from_secs(10));
         test_duration.run().unwrap();
+        let mut path = env::current_dir().unwrap();
+        path.push("src/tests/unit/services/history/history.yaml");
+        println!("working path: \n\t{:?}", env::current_dir().unwrap());
+        let app = App::new(path.display().to_string());
+        app.run().unwrap();
+
         // assert!(result == target, "\nresult: {:?}\ntarget: {:?}", result, target);
         test_duration.exit();
     }
