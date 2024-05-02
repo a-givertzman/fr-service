c42715bbd93267656e4c5311839aa14f9e955d81
a-givertzman <a.givertzman@icloud.com>
2024-05-02 10:53:12 +0300

    Packaging | action

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::path::PathBuf;
 use clap::Parser;
 use debugging::session::debug_session::{Backtrace, DebugSession, LogLevel};
 use log::error;
@@ -15,9 +15,10 @@ mod tcp;
 fn main() {
     DebugSession::init(LogLevel::Debug, Backtrace::Short);
     let cli = Cli::parse();
-    let path = env::current_dir().unwrap();
-    println!("main | working path: \n\t{:?}", path);
-    let path = path.join(cli.config.unwrap_or("config.yaml".to_owned()));
+    let path = cli.config.map_or_else(
+        || PathBuf::from("config.yaml"),        // || std::env::current_dir().unwrap().join("config.yaml"),
+        PathBuf::from
+    );
     let app = App::new(path);
     if let Err(err) = app.run() {
         error!("main | Error: {:#?}", err);
