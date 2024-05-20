64264a01c818f39c097ab411b767a9a6b887d22a
a-givertzman <a.givertzman@icloud.com>
2024-05-20 20:25:45 +0300

    Back | Task | Recorder - FnRisingEdge

--- src/services/task/nested_function/edge_detection/fn_rising_edge.rs
+++ src/services/task/nested_function/edge_detection/fn_rising_edge.rs
@@ -1,40 +1,43 @@
 use log::debug;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use crate::{
-    core_::{point::point_type::PointType, types::fn_in_out_ref::FnInOutRef},
+    core_::{point::{point::Point, point_type::PointType}, types::{bool::Bool, fn_in_out_ref::FnInOutRef}},
     services::task::nested_function::{
-        fn_::{FnInOut, FnIn, FnOut},
+        fn_::{FnIn, FnInOut, FnOut},
         fn_kind::FnKind,
     },
 };
 ///
-/// Function just doing debug of value coming from input
+/// Function | Returns true one tic (single computation cycle)
+/// if input value rising false -> true (0 (or any negative) -> any positive)
 #[derive(Debug)]
-pub struct FnDebug {
+pub struct FnRisingEdge {
     id: String,
     kind: FnKind,
     input: FnInOutRef,
+    prev: Point<Bool>,
 }
 ///
 /// 
-impl FnDebug {
+impl FnRisingEdge {
     ///
-    /// Creates new instance of the FnDebug
+    /// Creates new instance of the FnRisingEdge
     #[allow(dead_code)]
     pub fn new(parent: impl Into<String>, input: FnInOutRef) -> Self {
         Self { 
-            id: format!("{}/FnDebug{}", parent.into(), COUNT.fetch_add(1, Ordering::Relaxed)),
+            id: format!("{}/FnRisingEdge{}", parent.into(), COUNT.fetch_add(1, Ordering::Relaxed)),
             kind: FnKind::Fn,
             input,
+            prev: Point::new_bool(0, "inner", false)
         }
     }    
 }
 ///
 /// 
-impl FnIn for FnDebug {}
+impl FnIn for FnRisingEdge {}
 ///
 /// 
-impl FnOut for FnDebug { 
+impl FnOut for FnRisingEdge { 
     //
     fn id(&self) -> String {
         self.id.clone()
@@ -50,7 +53,23 @@ impl FnOut for FnDebug {
     //
     //
     fn out(&mut self) -> PointType {
-        let value = self.input.borrow_mut().out();
+        let input = self.input.borrow_mut().out();
+        debug!("{}.out | input: {:#?}", self.id, input);
+        let (name, value, tx_id, timestamp, status, cot) = match input {
+            PointType::Bool(point) => (point.name, point.value.0, point.tx_id, point.timestamp, point.status, point.cot),
+            PointType::Int(point) => (point.name, point.value > 0, point.tx_id, point.timestamp, point.status, point.cot),
+            PointType::Real(point) => (point.name, point.value > 0.0, point.tx_id, point.timestamp, point.status, point.cot),
+            PointType::Double(point) => (point.name, point.value > 0.0, point.tx_id, point.timestamp, point.status, point.cot),
+            PointType::String(_) => panic!("{}.out | String input value does not supported", self.id),
+        };
+        let value = PointType::Bool(Point::new(
+            tx_id,
+            &name,
+            Bool(value & (! self.prev.value.0)),
+            status,
+            cot,
+            timestamp,
+        ));
         debug!("{}.out | value: {:#?}", self.id, value);
         value
     }
@@ -62,7 +81,7 @@ impl FnOut for FnDebug {
 }
 ///
 /// 
-impl FnInOut for FnDebug {}
+impl FnInOut for FnRisingEdge {}
 ///
-/// Global static counter of FnDebug instances
+/// Global static counter of FnRisingEdge instances
 static COUNT: AtomicUsize = AtomicUsize::new(1);
