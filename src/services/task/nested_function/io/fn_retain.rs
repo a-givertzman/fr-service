35c6258373aa704aae4dda27b68947fc80fb8792
a-givertzman <a.givertzman@icloud.com>
2024-05-22 21:37:57 +0300

    Task | FnRetain

--- src/services/task/nested_function/io/fn_retain.rs
+++ src/services/task/nested_function/io/fn_retain.rs
@@ -2,7 +2,7 @@ use std::{env, fs, io::Write, path::{Path, PathBuf}, sync::{atomic::{AtomicUsize
 use log::{debug, error, warn};
 use concat_string::concat_string;
 use crate::{
-    conf::point_config::{point_config::PointConfig, point_config_type::PointConfigType},
+    conf::point_config::{name::Name, point_config::PointConfig, point_config_type::PointConfigType},
     core_::{
         point::{point::Point, point_tx_id::PointTxId, point_type::PointType}, 
         types::{bool::Bool, fn_in_out_ref::FnInOutRef}
@@ -20,6 +20,7 @@ use crate::{
 #[derive(Debug)]
 pub struct FnRetain {
     id: String,
+    parent_name: Name,
     tx_id: usize,
     kind: FnKind,
     enable: Option<FnInOutRef>,
@@ -33,13 +34,15 @@ impl FnRetain {
     ///
     /// Creates new instance of the FnRetain
     /// - parent - the name of the parent entitie
+    /// - name - the name of the parent
     /// - enable - boolean (numeric) input enables the storing if true (> 0)
     /// - key - the key to store Point with (full path: ./assets/retain/App/TaskName/key.json)
     /// - input - incoming Point's
-    pub fn new(parent: impl Into<String>, enable: Option<FnInOutRef>, key: String, input: FnInOutRef) -> Self {
+    pub fn new(parent: impl Into<String>, parent_name: Name, enable: Option<FnInOutRef>, key: String, input: FnInOutRef) -> Self {
         let self_id = format!("{}/FnRetain{}", parent.into(), COUNT.fetch_add(1, Ordering::Relaxed));
         Self {
             id: self_id.clone(),
+            parent_name,
             tx_id: PointTxId::fromStr(&self_id),
             kind: FnKind::Fn,
             enable,
@@ -50,16 +53,18 @@ impl FnRetain {
     }
     ///
     /// Writes Point value to the file:
-    fn write(&self, point: &PointType) -> Result<(), String> {
-        let path = match self.path {
+    fn write(&mut self, point: &PointType) -> Result<(), String> {
+        let new_path;
+        let path = match &self.path {
             Some(path) => Ok(path),
             None => {
-                let path = Name::new("assets/retain/", name.join()).join().trim_start_matches('/')
+                let path = Name::new("assets/retain/", self.parent_name.join()).join();
+                let path = path.trim_start_matches('/');
                 match Self::create_dir(&self.id, path) {
                     Ok(path) => {
-                        let path = path.join(concat_string!(self.key, ".json"));
-                        self.path = Some(path);
-                        Ok(path)
+                        new_path = path.join(concat_string!(self.key, ".json"));
+                        self.path = Some(new_path.clone());
+                        Ok(&new_path)
                     }
                     Err(err) => Err(concat_string!(self.id, ".write | Error: {}", err)),
                 }
@@ -72,7 +77,7 @@ impl FnRetain {
                     PointType::Int(point) => point.value.to_string(),
                     PointType::Real(point) => point.value.to_string(),
                     PointType::Double(point) => point.value.to_string(),
-                    PointType::String(point) => point.value,
+                    PointType::String(point) => point.value.clone(),
                 };
                 match fs::OpenOptions::new().truncate(true) .create(true).write(true).open(&path) {
                     Ok(mut f) => {
@@ -151,7 +156,7 @@ impl FnOut for FnRetain {
         };
         let point = self.input.borrow_mut().out();
         if enable {
-            self.send(point.clone());
+            self.write(&point);
         }
         point
     }
