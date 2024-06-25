5e4cb0dbc118e72136e1a261d57b33e63e61e54d
a-givertzman <a.givertzman@icloud.com>
2024-06-25 13:12:08 +0300

    Services | RetainPointId fixed

--- src/services/task/nested_function/fn_point_id.rs
+++ src/services/task/nested_function/fn_point_id.rs
@@ -1,6 +1,6 @@
 use hashers::fx_hash::FxHasher;
 use indexmap::IndexMap;
-use log::debug;
+use log::{debug, trace};
 use std::{hash::BuildHasherDefault, sync::atomic::{AtomicUsize, Ordering}};
 use concat_string::concat_string;
 use crate::{
@@ -64,8 +64,7 @@ impl FnOut for FnPointId {
     //
     fn out(&mut self) -> PointType {
         let point = self.input.borrow_mut().out();
-        debug!("{}.out | points: {:#?}", self.id, self.points);
-        debug!("{}.out | input: {:?}", self.id, point);
+        trace!("{}.out | input: {:?}", self.id, point);
         match self.points.get(&point.name()) {
             Some(id) => {
                 debug!("{}.out | ID: {:?}", self.id, id);
